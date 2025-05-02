dc9a7fd5664c10ce69bcab8e06afba18176d452c
study s <ohvelynuna@gmail.com>
2025-05-03 00:34:38 +1000

    middleware place

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import type { NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
   const host = req.headers.get("host");
   if (host === "sydneyot.com" && req.nextUrl.pathname.startsWith("/signpad")) {
-    return NextResponse.redirect(
+    return NextResponse.rewrite(
       `https://effulgent-lolly-8462e0.netlify.app${req.nextUrl.pathname}`,
     );
   }
