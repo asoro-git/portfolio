051b1f8c928a11b6a6a01accd90e866d29675adf
study s <ohvelynuna@gmail.com>
2025-05-03 20:21:14 +1000

    index page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,30 @@
+"use client";
 import Link from "next/link";
 export default function Home() {
     return (
         <>
             <div
-                className="h-full flex w-1/2 flex-col items-center justify-center"
+                className="text-center font-bold w-2/3 h-1/3  flex flex-col items-center justify-end"
+                style={{ fontSize: "30px" }}
+            >
+                SydneyOT.com
+                <br />
+                <br /> Click below to try out
+                <Link className="border rounded" href={"/signpad"}>
+                    Signature Pad
+                </Link>
+            </div>
+            <div
+                className="h-1/3 flex flex-col items-center justify-end"
                 style={{ fontSize: "15px" }}
             >
-                <div className="text-center flex flex-wrap justify-center items-center gap-x-2">
+                <div className="text-center flex flex-wrap justify-center items-end gap-x-2">
                     <div>Copyright ©{new Date().toISOString().slice(0, 4)} Sydney OT.</div>
                     <div>All rights reserved.</div>
                 </div>
                 <div className="text-center flex flex-wrap justify-center items-center gap-x-2">
                     Site made by Stanley
                 </div>
-            </div>
-            <div
-                className="font-bold h-52 flex flex-col items-center justify-center"
-                style={{ fontSize: "15px" }}
-            >
-                <p className="font-extrabold ">Useful links</p>
-                <Link className="underline" href={"/signpad"}>
-                    Signature Pad
-                </Link>
             </div>
         </>
     );
