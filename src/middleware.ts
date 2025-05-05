a0fec3c61939734a1929b83e02fd70dda2f8d0c4
study s <ohvelynuna@gmail.com>
2025-05-05 14:07:19 +1000

    case sensitive middleware fix (not sensitive after fix)

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
     const host = req.headers.get("host");
-    if (host === "sydneyot.com" && req.nextUrl.pathname.startsWith("/signpad")) {
+    if (host === "sydneyot.com" && req.nextUrl.pathname.toLowerCase().startsWith("/signpad")) {
         return NextResponse.rewrite(
             `https://effulgent-lolly-8462e0.netlify.app${req.nextUrl.pathname}`,
         );
